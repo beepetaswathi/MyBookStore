3deb541927bc52036c155129b11d2c15a4f7c780
Swathi Beepeta <123126383+beepetaswathi@users.noreply.github.com>
2023-10-06 17:31:50 -0500

    created Home and login layout page

---
+++ app/MyBookStore/app/src/main/java/com/example/mybookstore/Myinventory.java
@@ -0,0 +1,14 @@
+package com.example.mybookstore;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Myinventory extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.myinventory);
+    }
+}
